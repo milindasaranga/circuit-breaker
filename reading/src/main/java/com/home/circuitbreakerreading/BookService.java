d66adf52ed81300678ab0a08aab4e7b292a09399
Milinda Saranga <milindasaranga@Milindas-MacBook-Pro.local>
2020-07-12 02:35:11 +0400

    add rest api and consumer

---
+++ reading/src/main/java/com/home/circuitbreakerreading/BookService.java
@@ -0,0 +1,29 @@
+package com.home.circuitbreakerreading;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Service
+public class BookService{
+
+    private final RestTemplate restTemplate;
+
+    public BookService(RestTemplate rest){
+        this.restTemplate = rest;
+    }
+
+    @HystrixComman(fallbackMethod = "reliable")
+    public String readingList(){
+        URI uri= URI.create("http://localhsot:8090/recommended");
+        
+        return this.restTemplate.getForObject(uri, String.class);
+    }
+
+    public String reliable(){
+        return "Cloud native java (0'Reilly)";
+    }
+
+}
--- reading/src/main/java/com/home/circuitbreakerreading/CircuitBreakerReadingApplication.java
+++ reading/src/main/java/com/home/circuitbreakerreading/CircuitBreakerReadingApplication.java
@@ -2,9 +2,18 @@ package com.home.circuitbreakerreading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+@EnableCircuitBreaker
+@RestController
 @SpringBootApplication
 public class CircuitBreakerReadingApplication {
+
+	@Autowired
+	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CircuitBreakerReadingApplication.class, args);
